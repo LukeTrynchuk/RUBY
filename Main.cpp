0c65b2eff15a754a47f908128c10573358fd996f
Luke Trynchuk <luke123t@gmail.com>
2017-03-12 18:23:39 -0300

    Changed Main.cpp to ask for a name

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>					//Add the io capabilities
+#incluse <string>
 
 void main()
 {
-	std::cout << "Hello World" << std::endl;	//Send out hello world
+	std::cout << "Hello, what is your name?" << std::endl;	//Send out hello world
+	std::string Name = "";
+	std::cin >> Name;
+
+	std::cout << "Nice to meet you " + Name << std::endl;
 	system("pause");				//Wait for some input
 }
