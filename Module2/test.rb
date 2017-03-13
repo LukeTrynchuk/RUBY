b10669b92008b055ad2f21e1e4cc618a153c5736
Luke Trynchuk <luke123t@gmail.com>
2017-03-12 21:57:11 -0300

    The control structures in Module 2 Explained

--- Module2/test.rb
+++ Module2/test.rb
@@ -1,1 +1,29 @@
-puts 3
+caller_sentiment = 78;	#How well did that call go?
+
+						#Give the user feedback on the call
+
+if caller_sentiment <= 50
+	puts "That call could have gone better";
+elsif caller_sentiment <= 80
+	puts "It was good, but could improve";
+else
+	puts "It was great, keep it up";
+end
+
+a = 0;
+b = 200;
+c = 0;
+
+for i in a..b
+    c += i if i % 2 == 0;
+end
+
+puts c;
+
+
+
+
+
+
+
+
